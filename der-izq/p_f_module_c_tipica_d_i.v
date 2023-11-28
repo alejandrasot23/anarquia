379f70d420db76cbbcab65aed4e3a8e5a9ef14fc
alejandrasot23 <44853228+alejandrasot23@users.noreply.github.com>
2023-11-28 08:51:22 -0600

    Update p_f_module_c_tipica_d_i.v
    cambios celda inicial

--- der-izq/p_f_module_c_tipica_d_i.v
+++ der-izq/p_f_module_c_tipica_d_i.v
@@ -1,24 +1,35 @@
-module Celda_tipica_d_i #(
-    parameter N=3
+module Celda_tipica_d_i (
 
-) (
-    input wire a_p,
-    input wire b_p,
-    input wire x_p,
-    output wire p_x
+    input wire a_p, //entrada A, ultimo bit de la palabra A
+    input wire b_p, //entrada B, ultimo bit de la palabra B
+    input wire x_p, //entrada x, representa el estado inicial definido
+    output wire p_x //salida, resultado de la logica combinacional del modulo
 );
-wire notA;
+
+wire c1; //conecta la salida de not U1 a la entrada and U2
+wire c2; //salida de U2
+wire c3; //salida de U3
+wire c4; //salida de U4
+wire c5; //salida de U5
+wire c6; //salida de U6
+
+
+//Ecuacion celda tipica ~A&B | x&B | x&~A
 
     // Negar A
-    not U1 (a_p, notA);
+    not U1 (a_p, c1);
 
     //  ~A con B
-    and U2 (notA, b_p, p_x);
+    and U2 (c1, b_p, c2);
+
+    //  x con ~A
+    and U3 (c1, x_p, c3);
 
     //  x con B
-    and U3 (x_p, b_p, notA);
+    and U4 (x_p, b_p, c4);
 
     // Sumar
-    or U4 (p_x, notA, p_x);
+    or U5 (c2, c3, c5);
+    or U6 (c4, c5, c6);
 
 endmodule
