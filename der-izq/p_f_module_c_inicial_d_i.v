1a08a4f2ad9b9afbe36a08cf25e36bf66247dd6d
alejandrasot23 <44853228+alejandrasot23@users.noreply.github.com>
2023-11-28 08:50:54 -0600

    Update p_f_module_c_inicial_d_i.v
    cambios celda inicial

--- der-izq/p_f_module_c_inicial_d_i.v
+++ der-izq/p_f_module_c_inicial_d_i.v
@@ -1,18 +1,16 @@
-module Celda_inicial_d_i #(
-    parameter N=3
+module Celda_inicial_d_i (
 
-) (
-    input wire a_p,
-    input wire b_p,
-    input wire x_p,
-    output wire p_x
+    input wire a_p, //entrada A, ultimo bit de la palabra A
+    input wire b_p, //entrada B, ultimo bit de la palabra B
+    input wire x_p, //entrada x, representa el estado inicial definido
+    output wire p_x // salida, resultado de la logica combinacional del modulo
 );
-wire notA;
+wire notA; //conecta la salida de not U1 a la entrada and U2
 
     // Negar A
     not U1 (a_p, notA);
 
-    //  ~A con B
+    //  ~A por B
     and U2 (notA, b_p, p_x);
 
 endmodule
