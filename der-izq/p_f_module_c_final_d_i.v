4d947a45df30a60adff174b38e465282253959ac
alejandrasot23 <44853228+alejandrasot23@users.noreply.github.com>
2023-11-28 08:50:27 -0600

    Update p_f_module_c_final_d_i.v
    cambios celda final

--- der-izq/p_f_module_c_final_d_i.v
+++ der-izq/p_f_module_c_final_d_i.v
@@ -1,25 +1,32 @@
-module Celda_final_d_i#(
-    parameter N=3
+module Celda_final_d_i (
 
-) (
-    input wire a_p,
-    input wire b_p,
-    input wire x_p,
-    output wire p_x
+    input wire a_p, // Entrada A, último bit de la palabra A
+    input wire b_p, // Entrada B, último bit de la palabra B
+    input wire x_p, // Entrada x, representa el estado inicial definido
+    output wire p_x // Salida, resultado de la lógica combinacional del módulo
 );
 
-    wire notA;
+    wire c1; // Conecta la salida de not U1 a la entrada and U2
+    wire c2; // Salida de U2
+    wire c3; // Salida de U3
+    wire c4; // Salida de U4
+    wire c5; // Salida de U5
+    wire c6; // Salida de U6
 
     // Negar A
-    not not1 (notA, a_p);
+    not U1 (a_p, c1);
 
-    //  ~A con B
-    and andA (notA, b_p, p_x);
+    // ~A con B
+    and U2 (c1, b_p, c2);
 
-    //  x con B
-    and andB (x_p, b_p, notA);
+    // x con ~A
+    and U3 (x_p, c1, c3);
+
+    // x con B
+    and U4 (x_p, b_p, c4);
 
     // Sumar
-    or andC (p_x, notA, p_x);
+    or U5 (c2, c3, c5);
+    or U6 (c4, c5, p_x);
 
 endmodule
